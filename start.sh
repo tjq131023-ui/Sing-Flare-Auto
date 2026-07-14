7c5ee027109f2417e80f3a315c9ef8130a5da7e5
tjq131023-ui <tjq131023@gmail.com>
2026-07-14 21:53:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-# 替换配置文件中的 UUID
-sed -i "s/PASTE_YOUR_UUID_HERE/$UUID/g" config.json
+if [ -z "$PORT" ]; then
+  export PORT=8080
+fi
 
-# 后台运行 sing-box
-sing-box run -c config.json &
+if [ -z "$UUID" ]; then
+  export UUID="44ff9fab-3a9f-4bcc-9d16-34034cd9bac4"
+fi
 
-# 运行 Cloudflare Tunnel
-cloudflared tunnel --no-autoupdate run --token $ARGO_TOKEN
+if [ -z "$WSPATH" ]; then
+  export WSPATH="vless"
+fi
+
+echo "正在启动 Xray, 监听 PORT: $PORT, UUID: $UUID, WSPATH: $WSPATH"
+
+sed -i "s/UUID_PLACEHOLDER/$UUID/g" ./config.json
+sed -i "s/WSPATH_PLACEHOLDER/$WSPATH/g" ./config.json
+
+/usr/local/bin/xray -config ./config.json &
+
+if [ -z "$ARGO_AUTH" ]; then
+  echo "错误: 未配置 ARGO_AUTH 环境变量！"
+  exit 1
+fi
+
+echo "正在启动 cloudflared..."
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token "$ARGO_AUTH"
